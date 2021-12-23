cd3bd68abf686cd88671073b8c157d9ef19ddfb3
Marc Jansing <marc.jansing@innoq.com>
2021-12-23 13:44:37 +0100

    bump stimulus to v3

--- src/bundle.js
+++ src/bundle.js
@@ -1,3 +1,9 @@
-import { Application } from 'stimulus'
+import { Application } from "@hotwired/stimulus"
+window.Stimulus = Application.start()
+
+Stimulus.handleError = (error, message, detail) => {
+  console.warn(message, detail)
+  ErrorTrackingSystem.captureException(error)
+}
 
 console.log('hello faucet');
